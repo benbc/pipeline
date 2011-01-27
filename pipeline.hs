8d9b1086224bd82e4480d6aee5d673ca917167b7
benbc <ben@bridesmere.com>
2011-01-27 22:42:40 +0000

    Rename hs to be consistent with js.

--- pipeline.hs
+++ pipeline.hs
@@ -12,8 +12,8 @@ fetch' _ callback = do callback "prefix"
 converter :: (a -> b) -> Action a -> Action b
 converter conversion action callback =  action (callback . conversion)
 
-sequencer :: [Action a] -> Action [a]
-sequencer actions callback = f actions callback []
+aggregator :: [Action a] -> Action [a]
+aggregator actions callback = f actions callback []
  where f [] callback as = do callback as
                              return ()
        f (action:actions) callback as = action (\a -> f actions callback (a:as))
@@ -21,15 +21,17 @@ sequencer actions callback = f actions callback []
 concatenator :: Action [[a]] -> Action [a]
 concatenator = converter concat
 
+
+
 application = fetchAll operate
 
 operate :: String -> IO()
 operate = print
 
-fetchData = (converter show) . (converter sum) . concatenator . sequencer $ [fetch "first", fetch "second"]
+fetchData = (converter show) . (converter sum) . concatenator . aggregator $ [fetch "first", fetch "second"]
 
 fetchData' = fetch' "other"
 
-fetchAll = (converter join) . sequencer $ [fetchData, fetchData']
+fetchAll = (converter join) . aggregator $ [fetchData, fetchData']
 
 join xs = xs!!0 ++ xs!!1
